3e8891671374387f09c6457d38f0cc32ed2ab4c1
Victor Garcia <76071376+TortitasT@users.noreply.github.com>
2023-12-28 19:57:15 +0100

    Feat: login only when token expired (#1)
    * feat: login only when token expires WIP
    
    * fix: login when unauthorized
    
    * chore: wip
    
    * feat: login working

---
+++ lua/langkeeper/storage.lua
@@ -0,0 +1,86 @@
+local PATH_SECRETS = vim.fn.stdpath("data") .. "/langkeeper_secrets.json"
+local PATH_CONFIG = vim.fn.stdpath("config") .. "/langkeeper.json"
+
+local read_items = function(path)
+  local file = io.open(path, "r")
+
+  local contents = "[]"
+
+  if file ~= nil then
+    contents = file:read("*a")
+    file:close()
+  end
+
+  local err, json = pcall(vim.fn.json_decode, contents)
+
+  if err == false then
+    return nil
+  end
+
+  if json == nil then
+    return nil
+  end
+
+  return json
+end
+
+local find_item = function(path, key)
+  local configs = read_items(path)
+
+  if configs == nil then
+    return nil, nil
+  end
+
+  for i, c in pairs(configs) do
+    if c.key == key then
+      return i, c
+    end
+  end
+
+  return nil, nil
+end
+
+local store_item = function(path, item)
+  local file = io.open(path, "w")
+  local configs = read_items(path)
+
+  if file == nil then
+    return false
+  end
+
+  if configs == nil then
+    configs = {}
+  end
+
+  local found_index, _ = find_item(item.key)
+  if found_index ~= nil then
+    configs[found_index] = item
+  else
+    table.insert(configs, item)
+  end
+
+  file:write(vim.fn.json_encode(configs))
+  file:close()
+
+  return true
+end
+
+local M = {}
+
+M.find_config = function(key)
+  return find_item(PATH_CONFIG, key)
+end
+
+M.store_config = function(config)
+  return store_item(PATH_CONFIG, config)
+end
+
+M.find_secret = function(key)
+  return find_item(PATH_SECRETS, key)
+end
+
+M.store_secret = function(secret)
+  return store_item(PATH_SECRETS, secret)
+end
+
+return M
